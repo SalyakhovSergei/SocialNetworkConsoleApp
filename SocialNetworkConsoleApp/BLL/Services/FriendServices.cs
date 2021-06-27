c80ee4093761c5bdabdb39f80a4150ceccd852ed
SergeiSalyakhov <salyakhovsergei@gmail.com>
2021-06-27 21:53:29 +1000

    friend, friendservice

---
+++ SocialNetworkConsoleApp/BLL/Services/FriendServices.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using SocialNetworkConsoleApp.BLL.Exceptions;
+using SocialNetworkConsoleApp.BLL.Models;
+using SocialNetworkConsoleApp.DAL.Entities;
+using SocialNetworkConsoleApp.DAL.Repositories;
+
+namespace SocialNetworkConsoleApp.BLL.Services
+{
+    public class FriendServices
+    {
+        IFriendRepository friendRepository;
+        private IUserRepository userRepository;
+
+        public FriendServices()
+        {
+            friendRepository = new FriendRepository();
+            userRepository = new UserRepository();
+        }
+
+        public void AddFriend(Friend friend)
+        {
+            if (String.IsNullOrEmpty(friend.Email))
+                throw new UserNotFoundException();
+
+            var findFriend = this.userRepository.FindByEmail(friend.Email);
+            if (findFriend is null) throw new UserNotFoundException();
+
+            var friendEntity = new FriendEntity()
+            {
+                FriendId = friend.FriendId,
+                UserId = friend.UserId
+            };
+
+            if (this.friendRepository.Create(friendEntity) == 0)
+                throw new Exception();
+        }
+    }
+}
